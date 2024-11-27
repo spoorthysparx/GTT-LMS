11ce6adf007d3d4b6a2056d24009e868362b6a34
Cibi Sri Balaji M <cibisribalaji.kgm@gmail.com>
2024-11-27 13:44:08 +0530

    changes in userCoupon Repo

--- src/main/java/com/hexaware/gtt/lms/repositories/UserCouponRepository.java
+++ src/main/java/com/hexaware/gtt/lms/repositories/UserCouponRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hexaware.gtt.lms.entities.UserCoupons;
 
-public interface UserCouponRepository extends JpaRepository<UserCoupons, UUID> {
+public interface UserCouponRepository extends JpaRepository<UserCoupons, String> {
 
 	UserCoupons findCouponByCouponCode(String couponCode);
 	boolean existsCouponByCouponCode(String couponCode);
