58beb77785fd1c624d01ec702aab1a3f98862c00
CibiSriBalaji <88920782+CibiSriBalaji@users.noreply.github.com>
2024-12-09 12:38:39 +0530

    Program update and delete methods added (#20)

--- src/main/java/com/hexaware/gtt/lms/dto/ProgramDto.java
+++ src/main/java/com/hexaware/gtt/lms/dto/ProgramDto.java
@@ -8,6 +8,7 @@ import com.hexaware.gtt.lms.entities.Coupons;
 import com.hexaware.gtt.lms.entities.Offers;
 
 public class ProgramDto {
+	private UUID programId;
 	private String programName;
 	 private UUID partnerId;
 	private boolean status;
@@ -21,9 +22,11 @@ public class ProgramDto {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public ProgramDto(String programName, UUID partnerId, boolean status, List<Coupons> coupons, List<Offers> offers,
-			LocalDateTime startDate, LocalDateTime endDate) {
+	
+	public ProgramDto(UUID programId, String programName, UUID partnerId, boolean status, List<Coupons> coupons,
+			List<Offers> offers, LocalDateTime startDate, LocalDateTime endDate) {
 		super();
+		this.programId = programId;
 		this.programName = programName;
 		this.partnerId = partnerId;
 		this.status = status;
@@ -33,58 +36,96 @@ public class ProgramDto {
 		this.endDate = endDate;
 	}
 
+
+	public UUID getProgramId() {
+		return programId;
+	}
+
+
+	public void setProgramId(UUID programId) {
+		this.programId = programId;
+	}
+
+
 	public String getProgramName() {
 		return programName;
 	}
+
+
 	public void setProgramName(String programName) {
 		this.programName = programName;
 	}
-	public boolean isStatus() {
-		return status;
-	}
-	public void setStatus(boolean status) {
-		this.status = status;
-	}
-	public List<Coupons> getCoupons() {
-		return coupons;
-	}
-	public void setCoupons(List<Coupons> coupons) {
-		this.coupons = coupons;
-	}
-	public List<Offers> getOffers() {
-		return offers;
-	}
-	public void setOffers(List<Offers> offers) {
-		this.offers = offers;
-	}
-	public LocalDateTime getStartDate() {
-		return startDate;
-	}
-	public void setStartDate(LocalDateTime startDate) {
-		this.startDate = startDate;
-	}
-	public LocalDateTime getEndDate() {
-		return endDate;
-	}
-	public void setEndDate(LocalDateTime endDate) {
-		this.endDate = endDate;
-	}
-	
-	
+
+
 	public UUID getPartnerId() {
 		return partnerId;
 	}
+
 
 	public void setPartnerId(UUID partnerId) {
 		this.partnerId = partnerId;
 	}
 
+
+	public boolean isStatus() {
+		return status;
+	}
+
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+
+	public List<Coupons> getCoupons() {
+		return coupons;
+	}
+
+
+	public void setCoupons(List<Coupons> coupons) {
+		this.coupons = coupons;
+	}
+
+
+	public List<Offers> getOffers() {
+		return offers;
+	}
+
+
+	public void setOffers(List<Offers> offers) {
+		this.offers = offers;
+	}
+
+
+	public LocalDateTime getStartDate() {
+		return startDate;
+	}
+
+
+	public void setStartDate(LocalDateTime startDate) {
+		this.startDate = startDate;
+	}
+
+
+	public LocalDateTime getEndDate() {
+		return endDate;
+	}
+
+
+	public void setEndDate(LocalDateTime endDate) {
+		this.endDate = endDate;
+	}
+
+
 	@Override
 	public String toString() {
-		return "ProgramDto [programName=" + programName + ", partnerId=" + partnerId + ", status=" + status
-				+ ", coupons=" + coupons + ", offers=" + offers + ", startDate=" + startDate + ", endDate=" + endDate
-				+ "]";
+		return "ProgramDto [programId=" + programId + ", programName=" + programName + ", partnerId=" + partnerId
+				+ ", status=" + status + ", coupons=" + coupons + ", offers=" + offers + ", startDate=" + startDate
+				+ ", endDate=" + endDate + "]";
 	}
+
+
+
 
 	
 	
