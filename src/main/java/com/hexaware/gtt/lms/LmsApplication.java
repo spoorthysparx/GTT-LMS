7ec6abc295c25d4b6fdbd94a37e92a012e155deb
Spoorthy Pateel <SpoorthyP@hexaware.com>
2024-12-12 10:03:28 +0530

    changes in services

--- src/main/java/com/hexaware/gtt/lms/LmsApplication.java
+++ src/main/java/com/hexaware/gtt/lms/LmsApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class LmsApplication {
@@ -17,6 +20,18 @@ public class LmsApplication {
 	@Bean
 	public ModelMapper  modelmappper() {
 		return new ModelMapper();
+	}
+	
+	@Bean
+	public CorsFilter corsFilter() {
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true); // Allow credentials like Authorization headers
+	    config.addAllowedOrigin("http://localhost:3000"); // Allow your frontend
+	    config.addAllowedHeader("*"); // Allow all headers
+	    config.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+	    source.registerCorsConfiguration("/**", config); // Apply this configuration to all endpoints
+	    return new CorsFilter(source);
 	}
 
 }
--- src/main/java/com/hexaware/gtt/lms/controllers/PartnerController.java
+++ src/main/java/com/hexaware/gtt/lms/controllers/PartnerController.java
@@ -8,6 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +30,7 @@ import jakarta.validation.Valid;
  
 @RestController
 @RequestMapping("/api/v1/lms/partner")
+@CrossOrigin(origins = "http://localhost:3000")
 @ResponseBody
 public class PartnerController {
 	private PartnerService partnerService;
