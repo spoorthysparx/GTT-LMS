b8867164b6fa9ba1d101327e2a1f6efb173cf6ce
ZaID <zaidfarookhi@yahoo.com>
2024-12-15 23:41:08 +0530

    update tiersoffersdto

--- src/main/java/com/hexaware/gtt/lms/dto/TiersOffersDto.java
+++ src/main/java/com/hexaware/gtt/lms/dto/TiersOffersDto.java
@@ -1,18 +1,24 @@
 package com.hexaware.gtt.lms.dto;
 
+import java.util.UUID;
+
 public class TiersOffersDto {
 	
 	private String currentTier;
 	private String nextTier;
 	private Double totalPoints;
 	private Double leftPointsToReachNextTier;
+	private UUID tierId;
+    private String colour;
 	
-	public TiersOffersDto(String currentTier, String nextTier, Double totalPoints, Double leftPointsToReachNextTier) {
+	public TiersOffersDto(String currentTier, String nextTier, Double totalPoints, Double leftPointsToReachNextTier, UUID tierId, String colour) {
 		super();
 		this.currentTier = currentTier;
 		this.nextTier = nextTier;
 		this.totalPoints = totalPoints;
 		this.leftPointsToReachNextTier = leftPointsToReachNextTier;
+		this.tierId = tierId;
+		this.colour = colour;
 	}
 	public TiersOffersDto() {
 		super();
@@ -42,6 +48,22 @@ public class TiersOffersDto {
 	public void setLeftPointsToReachNextTier(Double leftPointsToReachNextTier) {
 		this.leftPointsToReachNextTier = leftPointsToReachNextTier;
 	}
+
+    public UUID getTierId() {
+        return tierId;
+    }
+
+    public void setTierId(UUID tierId) {
+        this.tierId = tierId;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
 	
 	
 
